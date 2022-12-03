3c79c799f2c352b8ff6f2eb56c48420dfeb61de3
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-12-03 07:54:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dulquer:dulquer@dulquer.betpe5w.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://spiderman:spiderman@cluster0.czbwb.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "dulquer")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
